b24cc291219ff3525174dc1bdc7cffb76ed086ee
Alireza <alirezarafieieng@gmail.com>
2023-11-14 00:07:21 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set random seeds
+np.random.seed(2023)
+random_seed = 2023
+
+# Load the data
+data_fluid.overload_imputed = pd.read_csv("./data_fluid.overload_imputed.csv")
+
+# Calculate the number of imputed datasets
+total_datasets = len(data_fluid.overload_imputed) // 991
+
+# Split the DataFrame into individual datasets
+datasets = [data_fluid.overload_imputed.iloc[i*991:(i+1)*991] for i in range(total_datasets)]
+
+# Randomly select 10 datasets
+selected_datasets = np.random.choice(datasets, size=10, replace=False)
+
+# Combine the selected datasets into a single DataFrame
+data_fluid_overload = pd.concat(selected_datasets)
