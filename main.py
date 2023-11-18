c30c452e22a3d8b86931457e6dee9030da6cf0c9
Alireza <alirezarafieieng@gmail.com>
2023-11-17 20:06:36 -0500

    Commit

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb
 from sklearn.model_selection import StratifiedKFold, ParameterGrid
 from imblearn.over_sampling import SMOTE
+from sklearn.naive_bayes import GaussianNB, ComplementNB, BernoulliNB,MultinomialNB
+from Choquet import ChoquetIntegral
+from sklearn.metrics import roc_auc_score
 
 # Set random seeds
 np.random.seed(2023)
@@ -30,7 +33,8 @@ data_fluid_overload = pd.concat(selected_datasets)
 data = data_fluid_overload.copy()
 sex_cols = pd.get_dummies(data['SEX'])
 ICU_type_cols = pd.get_dummies(data['main_ICU_type'])
-data= data.drop(columns = ['SEX','main_ICU_type'],axis=1)
+label = data['fluid_overload']
+data= data.drop(columns = ['SEX','main_ICU_type', 'fluid_overload'],axis=1)
 data = pd.concat([sex_cols['Male'], ICU_type_cols['Medical'],data], axis=1)
 data['AKI_24h'] = data['AKI_24h'].map({'Yes': 1, 'No': 0})
 data['CRRT_24h'] = data['CRRT_24h'].map({'Yes': 1, 'No': 0})
@@ -39,51 +43,81 @@ data['Mechanical_ventilation'] = data['Mechanical_ventilation'].map({'Yes': 1, '
 data['Vasopressor_24h'] = data['Vasopressor_24h'].map({'Yes': 1, 'No': 0})
 data['Use_CI_24h'] = data['Use_CI_24h'].map({'Yes': 1, 'No': 0})
 
-# Define the classifiers and their parameter grids
-classifiers = {
-    'SVM': [SVC(), {'C': [0.2, 0.5, 0.8, 1.5, 3, 5, 10, 25, 50], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'degree': [2, 3, 4, 8], 'random_state': [random_state]}],
-    'Random Forest': [RandomForestClassifier(), {'n_estimators': [100, 150, 200, 300, 500, 1000, 1500, 3000], 'max_depth': [5, 8, 10, 12, 14, 18], 
-                      'min_samples_split': [1, 2, 4], 'min_samples_split': [2, 4, 5, 10], 'class_weight': [{0.0:1,1.0:1},{0.0:1,1.0:1.2},{0.0:1,1.0:1.5}], 'random_state': [random_state]}],
-    'XGBoost': [xgb.XGBClassifier, {'n_estimators': [100, 250, 500], 'max_depth': [5, 7, 12, 15], 'learning_rate': [0.01, 0.1], 'colsample_bytree': [0.6, 0.8, 1],
-                'gamma': [0, 0.1, 1], 'scale_pos_weight': [1, 1.1, 1.2, 1.5, 2], 'random_state': [random_state]}]
-}
-
+# Define the classifiers' parameter grids
 rf_grid = {'n_estimators': [100, 150, 200, 300, 500, 1000, 1500, 3000], 'max_depth': [5, 8, 10, 12, 14, 18], 
                       'min_samples_split': [1, 2, 4], 'min_samples_split': [2, 4, 5, 10], 'class_weight': [{0.0:1,1.0:1},{0.0:1,1.0:1.2},{0.0:1,1.0:1.5}], 'random_state': [random_state]}
 svm_grid = {'C': [0.2, 0.5, 0.8, 1.5, 3, 5, 10, 25, 50], 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'], 'degree': [2, 3, 4, 8], 'random_state': [random_state]}
 xgb_grid = {'n_estimators': [100, 250, 500], 'max_depth': [5, 7, 12, 15], 'learning_rate': [0.01, 0.1], 'colsample_bytree': [0.6, 0.8, 1],
                 'gamma': [0, 0.1, 1], 'scale_pos_weight': [1, 1.1, 1.2, 1.5, 2], 'random_state': [random_state]}
 
+# Search space for the meta-model
+dt_grid = {'max_depth': [3, 5, 10], 'min_samples_split': [2, 5, 10]}
+ada_grid = {'n_estimators': [50, 100, 200], 'learning_rate': [0.01, 0.1, 1]}
+
+meta_grids = {
+    'LogisticRegression': None,
+    'DecisionTree': dt_grid,
+    'AdaBoost': ada_grid,
+    'CustomModel': None  # 'None' indicates no hyperparameters for 'ch'
+}
 
 # Stratified K-Folds cross-validator
 cv = StratifiedKFold(n_splits=5)
+cv2 = StratifiedKFold(n_splits=5)
 
-# Iterate over classifiers and hyperparameters
+# Initialize the best score and best params
+best_score = 0
 best_params = {}
-for name, (classifier, param_grid) in classifiers.items():
-    best_score = 0
-    for params in ParameterGrid(param_grid):
-        classifier.set_params(**params)
-        scores = []
-        for train_idx, test_idx in cv.split(X, y):
-            X_train, X_test = X[train_idx], X[test_idx]
-            y_train, y_test = y[train_idx], y[test_idx]
-            
-            # Apply SMOTE
-            smote = SMOTE()
-            X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
-            
-            # Train the model
-            classifier.fit(X_train_smote, y_train_smote)
-            y_pred = classifier.predict(X_test)
-            scores.append(accuracy_score(y_test, y_pred))
-        
-        mean_score = np.mean(scores)
-        if mean_score > best_score:
-            best_score = mean_score
-            best_params[name] = params
 
-# Print the best parameters for each classifier
-for classifier in best_params:
-    print(f"Best parameters for {classifier}: {best_params[classifier]}")
-    print(f"Best cross-validation score for {classifier}: {best_score}")
+# Iterate over all combinations of hyperparameters
+for rf_params in ParameterGrid(rf_grid):
+    for svm_params in ParameterGrid(svm_grid):
+        for xgb_params in ParameterGrid(xgb_grid):
+            ensemble_scores = []
+
+            for train_idx, test_idx in cv.split(data, label):
+                X_train, X_test = data[train_idx], data[test_idx]
+                y_train, y_test = label[train_idx], label[test_idx]
+
+                for opt_idx, val_idx in cv2.split(X_train, y_train):
+                    X_opt, X_val = X_train[opt_idx], X_train[val_idx]
+                    y_opt, y_val = y_train[opt_idx], y_train[val_idx]  
+                    ensemble_scores_inner = []
+
+                    # Train each base model with the current set of parameters
+                    rf_model = RandomForestClassifier(**rf_params).fit(X_opt, y_opt)
+                    svm_model = SVC(**svm_params, probability=True).fit(X_opt, y_opt)
+                    xgb_model = xgb.XGBClassifier(**xgb_params).fit(X_opt, y_opt)
+
+                    # Generate predictions (probabilities) from each model
+                    rf_pred = rf_model.predict_proba(X_val)
+                    svm_pred = svm_model.predict_proba(X_val)
+                    xgb_pred = xgb_model.predict_proba(X_val)
+
+                    # Combine the predictions for the meta-model
+                    meta_features = np.column_stack([rf_pred, svm_pred, xgb_pred])
+
+                    # Train the meta-model
+                    meta_model = GaussianNB().fit(meta_features, y_val)
+
+                    # Evaluate the meta-model
+                    meta_model_pred = meta_model.predict(meta_features)
+                    accuracy = roc_auc_score(y_val, meta_model_pred)
+
+                    ensemble_scores_inner.append(accuracy)
+
+                # Average score of the inner loop
+                avg = np.append(np.mean(ensemble_scores_inner))
+
+            # Average score for this combination
+            avg_score = np.mean(avg)
+
+            # Update best score and parameters if current score is better
+            if avg_score > best_score:
+                best_score = avg_score
+                best_params = {'rf': rf_params, 'svm': svm_params, 'xgb': xgb_params}
+
+# Print the best score and corresponding parameters
+print("Best Ensemble Model Accuracy:", best_score)
+print("Best Parameters:", best_params)
+
